8f82333183a65dd2bde7d6c82086fee3eae8bac8
kwakdong416 <142363587+kwakdong416@users.noreply.github.com>
2023-11-23 14:23:39 +0900

    #2.2 Variables

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-alert("hi!");
-/* 브라우저는 js 파일을 여는 데 적합하지 않다. 파일을 실행시키는 것이 아니라 열어보는 것 뿐이기 때문
-    이는 css 파일도 마찬가지다. 브라우저로 실행시켜 볼 수 있는 것은 html 파일 뿐이다. */
+console.log("ㅁㄴㅇ");
+/*
+콘솔에 값을 log 즉  하고있다. 
+
+*/
+
+console.log(10 + 6);
+console.log(10 * 6);
+console.log(10 / 6);
+
+/*  
+javascript는 위에서 아래로 코드를 읽는다.
+나누기를 하면 소숫점 자리까지 계산을 해준다.
+*/
+
+const a = 10;
+const b = 7;
+const myName = " Nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello" + myName);
+
+/* 
+const는 constant 즉 상수를 의미한다.
+*/
