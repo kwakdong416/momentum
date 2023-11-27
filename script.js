8338c252b824d668fa3e648d2b1f382f3172d5f9
kwakdong416 <142363587+kwakdong416@users.noreply.github.com>
2023-11-27 16:15:28 +0900

    #2.13 Conditionals

--- script.js
+++ script.js
@@ -1,77 +1,28 @@
-const calcaulator = {
-  plus: function (a, b) {
-    alert(a + b);
-  },
-  minus: function (a, b) {
-    alert(a - b);
-  },
-  divide: function (a, b) {
-    alert(a / b);
-  },
-  times: function (a, b) {
-    alert(a * b);
-  },
-  square: function (a, b) {
-    alert(a ** b);
-  },
-};
+const age = prompt("How old are you?");
 
-console.log(calcaulator.plus(2, 3));
+console.log(typeof "15", typeof parseInt("15"));
+console.log(age, parseInt(age));
+
 /*
-이 코드를 실행하면 alert에 의해 5 알림창이 뜨지만 적장 실제 plus 값은 undefined이다.
+prompt()는 사용자에게 창을 띄울 수 있도록 해주는 function이다.
+string을 출력한 후 string을 입력받을 수 있다. 숫자를 입력해도 string이 된다.
+
+parsInt는 string을 number로 바꿔주는 function이다. parseInt()가 적용된prompt()에 
+숫자가 아닌 다른 걸 입력하면 NaN이 뜬다.
+
+console.log(age);가 아직 작동하지 않은 채로 javscript는 우리를 기다리고 있다.
+prompt는 요즘 잘 안 쓰이는 function으로 css를 적용시킬 수도 없고 특정 브라우저
+에서는 아예 실행이 되지 않는다.
+
+요즘에는 html , css로 만든 각자만의 창을 사용한다.
+
+condition은 조건문을 의미한다. true와 false에 따라 조건문을 실행한다.
 */
 
-const age = 96;
-
-function calculateKrAge(ageOfForeigner) {
-  return ageOfForeigner + 2;
-}
-
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
+const age2 = parseInt(prompt("How old are you?"));
+console.log(age2);
 
 /*
-function을 사용할 때는 return을 적어줘야지만 변수내 값까지 변경시킬 수 있다.
-
-앞으로는 console에 출력하려고 function을  사용하지 않고 어떤 작업을 처리하고
-그 결과를 return하기 위해 function을 사용할 것이다.
-
-
-console에 값을 보여주는 것과 그 variable 내 값을 변경하는 것은 엄연히 다른 것이다.
-*/
-
-const calcaulator2 = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  square: function (a, b) {
-    return a ** b;
-  },
-};
-
-const plusResult = calcaulator2.plus(5, 2);
-const minusResult = calcaulator2.minus(plusResult, 10);
-const timesResult = calcaulator2.times(10, minusResult);
-const divideResult = calcaulator2.divide(minusResult, plusResult);
-const squreResult = calcaulator2.square(divideResult, minusResult);
-
-console.log(plusResult);
-
-/*
-function 내에서 console을 사용하지 않는 것은 외부에서 값을 투입하고  이를 
-return으로 돌려받는 것이 프로그램이 복잡해질 수록 개발 편이성이 훨씬 높기 때문이다.
-
-위 코드를 보면 서로가 서로를 의존하고 있다. 이는 코드의 흐름을 이해하는 데 매우 좋은 방법이다.
-
-브라우저 콘솔에 가서 각 변수명을 쳐보면 각 변수내에 있는 값을 볼 수 있다.
+funcion은 안에서부터 밖으로 실행된다.
+위 코드를 실행하면 NaN이 뜬다.
 */
