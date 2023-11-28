964b44b85c32b0e72bdca533f4f2e8359a6db483
kwakdong416 <142363587+kwakdong416@users.noreply.github.com>
2023-11-28 10:38:49 +0900

    #2.16 Recap

--- script.js
+++ script.js
@@ -10,13 +10,12 @@ if (isNaN(age) || age < 0) {
   console.log("You can drink.");
 } else if (age > 50 && age <= 80) {
   console.log("You should exercise.");
+} else if (age === 100) {
+  console.log("Wow you are wise.");
 } else if (age > 80) {
   console.log("You can do whatever you want.");
-} else {
-  console.log("You can't drink.");
 }
-
 /*
-위 코드에서 isNaN이 flase 이면 else if 가 실행되고 else if 도 false이면 else가 실행된다.
-else는 옵션일 뿐 필수적으로 적을 필요는 없다.
+else if 는 여러개가 동시에 실행될 수 없고 그 중 하나를 골라야 한다.
+&& , || 말고도 === 과 !==도 있다. , 이 recap만 특별히 파일 정리함
 */
