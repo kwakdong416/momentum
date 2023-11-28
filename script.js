d16913bebe8cf9fee1a88126bfa673133903f3ac
kwakdong416 <142363587+kwakdong416@users.noreply.github.com>
2023-11-28 15:23:43 +0900

    #3.1 HTML in Javascript

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title");
+console.log(title);
+console.dir(title);
 
-console.log(isNaN(age));
+/* 
+console.log(title); 코드는 콘솔에서
+document.getElementById("title");
+를 사용한 것과 동일한 결과물을 만들어낸다.
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number.");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink.");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise.");
-} else if (age === 100) {
-  console.log("Wow you are wise.");
-} else if (age > 80) {
-  console.log("You can do whatever you want.");
-}
-/*
-else if 는 여러개가 동시에 실행될 수 없고 그 중 하나를 골라야 한다.
-&& , || 말고도 === 과 !==도 있다. , 이 recap만 특별히 파일 정리함
+console.dir(title); 코드는 element를
+더 자세하게 보여준다. (별 차이는 모르겠다.)
+
+document.getElementsByClassName("title");
+class를 이용하는 방법도 있다.
 */
+
+/*
+javascript는 html element를 가져오긴 
+하지만 html 자체를 그대로 보여주지는 않는다.
+javascript에서 html을 표현하는 object를
+보여줄 뿐이다.
+
+document는 곧 html 문서를 의미한다.
+*/
+
+title.innerText = "I Got You.";
+
+/*
+이런 식으로 html 내 element를 변경할 수도 있다.
+*/
+
+console.log(title.id);
+console.log(title.className);
