52e6aca7310fe1e0fc898b6ed7421f8b0a5048ae
kwakdong416 <142363587+kwakdong416@users.noreply.github.com>
2023-11-28 10:17:58 +0900

    #2.15 Conditionals part Three

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const age = parseInt(prompt("How old are you?"));
 
 console.log(isNaN(age));
 
-if (isNaN(age)) {
-  console.log("Please write a number.");
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number.");
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink.");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise.");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
 } else {
-  console.log("Thank you for your writing your age.");
+  console.log("You can't drink.");
 }
 
 /*
-isNan()은 받은 값이 number인지 아닌지 boolean으로 알려준다.
-숫자이면 false 숫자가 아니면 true를 반환한다.
+위 코드에서 isNaN이 flase 이면 else if 가 실행되고 else if 도 false이면 else가 실행된다.
+else는 옵션일 뿐 필수적으로 적을 필요는 없다.
 */
