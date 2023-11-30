ac85e39d6285ce0da322cd1b83aca7f8f40c8dac
kwakdong416 <142363587+kwakdong416@users.noreply.github.com>
2023-11-30 14:36:54 +0900

    #3.4 Events part Two

--- script.js
+++ script.js
@@ -4,20 +4,7 @@ title.innerText = "New innerText";
 
 console.log(title);
 
-/*
-querySelectorAll을 사용하면 xx.xxx 식의 사용을 할 수 없기 떄문에 
-querySelector을 사용했다.  :first-child를 적용했기 때문에
-Grab me! 1만 New innerText로 innerText가 바뀐다.
-*/
-
 title.style.color = "blue";
-
-/*
-object 내 property를 변경할 수 있는 경우가 많지만 아닌 경우도 많다.
-콘솔에 가서 h1을 열어보면 h1에 대한 여러가지 설정을 볼 수 있는데
-이것들 하나하나가 전부 object이다. 위 코드의 경우 style 관련 설정 중
-color를 변경해 준 것이다.
-*/
 
 function handleTitleClick() {
   console.log("You clicked!");
@@ -26,6 +13,38 @@ function handleTitleClick() {
 
 title.addEventListener("click", handleTitleClick);
 
+console.dir(title);
+
 /*
-addEventListener를 이용해 특정 독장 수행시 함수를 실행시킨다.
+dir을 사용해 title이 사용할 수 있는 property를 확인할 수 있다.
+앞에 on이 붙는 것들이 event에 관련된 것들이다. on을 빼고 쓰면 된다.
+예를 들어 onclick이면 click이라고만 쓰면 된다.
+*/
+
+function handleMouseEnter() {
+  title.innerText = "mouse is here!";
+}
+
+title.addEventListener("mouseenter", handleMouseEnter);
+
+// 마우스를 타이틀 위에 올려놨을 때
+
+function handleMouseLeave() {
+  title.innerText = "mouse is gone!";
+}
+
+title.addEventListener("mouseleave", handleMouseLeave);
+
+// 마우스가 h1을 벗어났을 때
+
+/*
+
+h1 html element mdn
+
+https://www.google.com/search?q=h1+html+element+mdn&oq=h1+html+element+mdn&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBBzcyOGowajeoAgCwAgA&sourceid=chrome&ie=UTF-8
+
+에서 다양한 event에 대해 알아볼 수 있다.
+
+style 변화는 사실 javascript 보단 css를 활용하는 것이 낫다.
+
 */
