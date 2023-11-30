fca8033c8d0145bc55f46d7950b892c748f719cd
kwakdong416 <142363587+kwakdong416@users.noreply.github.com>
2023-11-30 15:29:52 +0900

    #3.5 More Events

--- script.js
+++ script.js
@@ -1,50 +1,69 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-title.innerText = "New innerText";
+h1.innerText = "New innerText";
 
-console.log(title);
+console.log(h1);
 
-title.style.color = "blue";
+h1.style.color = "blue";
 
 function handleTitleClick() {
   console.log("You clicked!");
-  title.style.color = "red";
+  h1.style.color = "red";
 }
 
-title.addEventListener("click", handleTitleClick);
-
-console.dir(title);
-
-/*
-dir을 사용해 title이 사용할 수 있는 property를 확인할 수 있다.
-앞에 on이 붙는 것들이 event에 관련된 것들이다. on을 빼고 쓰면 된다.
-예를 들어 onclick이면 click이라고만 쓰면 된다.
-*/
+console.dir(h1);
 
 function handleMouseEnter() {
-  title.innerText = "mouse is here!";
+  h1.innerText = "mouse is here!";
 }
-
-title.addEventListener("mouseenter", handleMouseEnter);
 
 // 마우스를 타이틀 위에 올려놨을 때
 
 function handleMouseLeave() {
-  title.innerText = "mouse is gone!";
+  h1.innerText = "mouse is gone!";
 }
 
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+  /* 
+  javascript로 가져올 수 있는 body 부분은 backgroundColor가 전부다.
+  document.div나 document.title 같은 건 사용할 수 없다.
+  다른 element는 querySelector나 getElementById 같은 걸 사용해야 한다.
+  */
+}
 
-// 마우스가 h1을 벗어났을 때
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS No wifi!");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOD!");
+}
+
+//title.addEventListener("click", handleTitleClick);
+h1.onclick = handleTitleClick;
+//title.addEventListener("mouseenter", handleMouseEnter);
+h1.onmouseenter = handleMouseEnter;
+h1.addEventListener("mouseleave", handleMouseLeave);
 
 /*
+title.onclick = handleMouseEnter;
+title.addEventListener(“mouseenter” , handleMouseEnter);
 
-h1 html element mdn
+위에 두 코드는 동일하나 addEventListener를 선호하는 이유는
+removeEventListener을 통해서 event listener을 제거할수있기 때문이다.
+*/
 
-https://www.google.com/search?q=h1+html+element+mdn&oq=h1+html+element+mdn&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBBzcyOGowajeoAgCwAgA&sourceid=chrome&ie=UTF-8
-
-에서 다양한 event에 대해 알아볼 수 있다.
-
-style 변화는 사실 javascript 보단 css를 활용하는 것이 낫다.
-
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
+/*
+windows 또한 object 처럼 javascript에서 기본적으로 제공하는 것이다.\
+windows에서 발생한 event이므로 window를 사용하는 것이다. h1에서 발생한
+이벤트이면 h1을 사용해야 한다.
 */
