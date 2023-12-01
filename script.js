e014bc4afd77fd2521650fc690eb30273e5c6512
kwakdong416 <142363587+kwakdong416@users.noreply.github.com>
2023-12-01 15:42:55 +0900

    #3.8 CSS in Javascript part Three

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const clickedClass = "active sexy-font";
   /*
-   <h1> 태그가 처음부터 가지고 있는 sexy-font를 보존시키기 위해 추가해줬다
-   사실 이 방법은 그리 좋은 방법은 아니다. sexy-font를 건드리지 않으면서 
-   active를 추가하는 방법이 필요하다. 이는 다음 강의에서 배운다.
-   */
-  if (h1.className === clickedClass) {
-    h1.className = "";
+  const clickedClass = "active";
+
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    h1.className = clickedClass;
+    h1.classList.add(clickedClass);
   }
   console.log(h1.className);
-}
-/* 
-styles.css에서 active 클래스에 대한 스타일을 설정했다. 
-굳이 clickedClass = "active";를 적어주는 이유는
-실수를 방지하기 위함이다. 
 
-잘못된 javascript 변수명을 사용하면 javascript가 알려줘서 
-실수를 방지할 수 있다. active를 한번만 복사하면 
-되는 것과 여러번 복사하면 되는 것은 실수 빈도 발생 숫자가 확 다르다.
-*/
+  h1.classList.contains(clickedClass)는 h1이 clickedClass를 포함하는 지만을 확인한다.
+  */
+  h1.classList.toggle("active");
+  console.log(h1.className);
+  /*
+   이 한 줄 만으로 위 주석처리 된 전체 코드를 대체할 수 있다.
+    한 줄만 쓰면 되기 때문에 위 const 부분까지 주석처리를 하였다.
+    여러번 반복해서 사용할 떄 const를 생성하면 된다.
+   */
+}
 
 h1.addEventListener("click", handleTitleClick);
